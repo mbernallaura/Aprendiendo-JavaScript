db50e6316358cfab2861636170059f70ab616162
mbernallaura <mbernallaura@gmail.com>
2022-08-03 21:08:54 -0500

    Terminando las funcionalidades mas implementadas con los objetos

--- assets/js/objeto-literal.js
+++ assets/js/objeto-literal.js
@@ -30,3 +30,34 @@ console.log('Vivo:', pj[x] );
 //Nombre de una llave diferente
 console.log('Ultima Pelicula:', pj["ultima-pelicula"]);
 
+//Borrar un llave valor dentro de un objeto
+delete pj.edad;
+
+//Transformar un objeto en un "array" para saber el posicionamiento de los llave valor
+const arrPj= Object.entries(pj);
+console.log(arrPj);
+console.log(arrPj[0]); //Sale como un array que tiene 2 posiciones, 0 = nombre, 1= Tony Stark
+console.log(arrPj[0][0]);
+
+//Agregar un nuevo llave valor
+pj.casado = 'Hola';
+console.log(pj);
+
+//Convertir el objeto para que no pueda cambiar sus valores o agregar nuevos
+//Tocaria hacer el mismo procedimiento con los objetos dentro del objeto
+Object.freeze(pj);
+pj.residente = 'ALV';
+console.log(pj);
+
+//Transformar el objeto en un arreglo pero solo saber el nombre de las llaves
+const llave = Object.getOwnPropertyNames(pj)
+console.log(llave);
+
+//Transformar el objeto en un arreglo pero solo saber el contenid de los valores llaves
+const valor = Object.values(pj);
+console.log(valor);
+
+
+
+
+
