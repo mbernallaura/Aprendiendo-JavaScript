01cd828491dc6c8413c7d9d24b969ed8877b6561
mbernallaura <mbernallaura@gmail.com>
2022-08-04 20:05:20 -0500

    Returns en funciones y funciones de flecha

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,4 +1,5 @@
 //Definicion metodo: Es una funcion que se encuentra dentro de un objeto
+//CUANDO UNA FUNCION NO TIENE UN RETURN explicito, esto por defecto RETORNA UNDEFINED
 function saludar() {
     console.log('Hola mundo');
 }
@@ -26,11 +27,35 @@ saludarNombre2('Aleja');
 const saludarFlecha = () =>{
     console.log('Hola flecha');
 }
-
 saludarFlecha();
 
 const saludarFlecha2 = (nombre) =>{
     console.log('Hola ' + nombre);
 }
-
 saludarFlecha2('Flecha 2');
+
+//Funciones poniendo return
+function sumar(x, y) {
+    return x + y;
+}
+console.log(sumar(4,2));
+
+//Funcion flecha con return
+const sumar2 = (x, y) =>{
+    return x + y;
+}
+console.log(sumar2(6,8));
+
+//Hacer la funcion flecha anterior, pero de forma corta
+//SE PUEDE HACER ESTO PORQUE SOLO ESTA LA LINEA DEL RETURN, NO FUNCIONA SI HUBIERAS MAS LINEAS DE CODIGO
+const sumar2_1 = (x, y) => x + y;
+console.log(sumar2_1(4,4));
+
+function getAleatorio(){
+    return Math.random();
+}
+console.log(getAleatorio());
+
+//Transformando la anterior funcion en funcion de flecha
+const getAleatorio2 = () => Math.random();
+console.log(getAleatorio2());
