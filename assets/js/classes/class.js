ef2ede1cc7a54c8840b86296fe3429472406bfe9
mbernallaura <mbernallaura@gmail.com>
2022-08-30 15:17:02 -0500

    propiedades set y gets de una clase

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -2,12 +2,23 @@ class Persona{
     nombre = '';
     codigo = '';
     frase  = '';
+    comida = '';
 
     //Se puede asignar valores por defecto a los atributos desde el contructor
     constructor(nombre = 'Sin nombre', codigo = 'Sin codigo', frase = 'Sin frase'){
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase  = frase;
+    }
+
+    //Set: estabecer un valor
+    set setComidaFavorita(comida){
+        this.comida = comida.toUpperCase();
+    }
+
+    //get: Obtener valor
+    get getComidaFavorita(){
+        return `La commida favorita de ${this.codigo} es ${this.comida}`;
     }
 
     quienSoy(){
@@ -26,3 +37,6 @@ const tonny = new Persona();
 console.log(peter);
 console.log(tonny);
 peter.miFrase();
+
+peter.setComidaFavorita = 'El pie de cereza de la tia May';
+console.log(peter.getComidaFavorita);
