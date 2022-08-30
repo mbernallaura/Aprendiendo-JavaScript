857c6a5bc5cb7d10f091ae911f9cc98082c1cf62
mbernallaura <mbernallaura@gmail.com>
2022-08-29 19:50:41 -0500

    Se crea la primera clase estructurada con sus respectivos metodos y llamados, dentro y fuera de la clase

---
+++ assets/js/classes/class.js
@@ -0,0 +1,28 @@
+class Persona{
+    nombre = '';
+    codigo = '';
+    frase  = '';
+
+    //Se puede asignar valores por defecto a los atributos desde el contructor
+    constructor(nombre = 'Sin nombre', codigo = 'Sin codigo', frase = 'Sin frase'){
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.frase  = frase;
+    }
+
+    quienSoy(){
+        console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`);
+    }
+
+    miFrase(){
+        //Se puede llamar los metos dentro de la misma clase de esta manera:
+        this.quienSoy();
+        console.log(`${this.codigo} dice: ${this.frase}`);
+    }
+}
+
+const peter = new Persona('Peter Parker', 'Spiderman', 'No digas mmdas meriyein');
+const tonny = new Persona();
+console.log(peter);
+console.log(tonny);
+peter.miFrase();
